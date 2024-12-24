fb567e9245226472a94ae8a61761c5b87fb1cc14
Al <ali.al81@ui.ac.id>
2024-12-24 10:35:36 +0700

    Routing on no login

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -15,9 +15,10 @@ export const authConfig = {
       const isOnChat = nextUrl.pathname.startsWith('/');
       const isOnRegister = nextUrl.pathname.startsWith('/register');
       const isOnLogin = nextUrl.pathname.startsWith('/login');
+      const baseUrl = process.env.NEXTAUTH_URL || nextUrl.origin;
 
       if (isLoggedIn && (isOnLogin || isOnRegister)) {
-        return Response.redirect(new URL('/', nextUrl as unknown as URL));
+        return Response.redirect(new URL('/', baseUrl));
       }
 
       if (isOnRegister || isOnLogin) {
@@ -30,7 +31,7 @@ export const authConfig = {
       }
 
       if (isLoggedIn) {
-        return Response.redirect(new URL('/', nextUrl as unknown as URL));
+        return Response.redirect(new URL('/', baseUrl));
       }
 
       return true;
