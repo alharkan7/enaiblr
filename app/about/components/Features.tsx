15fe1ca97163257209164868f4fe34d04fefa62d
Al <ali.al81@ui.ac.id>
2025-01-07 15:25:17 +0700

    Copy features

--- app/about/components/Features.tsx
+++ app/about/components/Features.tsx
@@ -11,7 +11,7 @@ const Features = () => {
           <h2 className="text-3xl font-bold mb-4">
             {" "}
             <span className="bg-gradient-to-r from-blue-600 to-cyan-500 bg-clip-text text-transparent">
-              Fitur AI Lengkap
+              Fitur AI Unlimited
             </span> {" "}
             yang Kamu Butuhkan
           </h2>
