67f021c631d41779a35573139fe46df03c1dae05
Al Harkan <ali.al81@ui.ac.id>
2025-04-12 00:21:59 +0700

    Update header.tsx

--- app/(landing-page)/components/ui/header.tsx
+++ app/(landing-page)/components/ui/header.tsx
@@ -31,10 +31,10 @@ export default function Header() {
             )}
             <li>
               <Link
-                href={session ? "/apps" : "/register"}
+                href="/apps"
                 className="btn-sm bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] py-[5px] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%]"
               >
-                {session ? "Access" : "Register"}
+                Apps
               </Link>
             </li>
           </ul>
